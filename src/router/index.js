aac24e0cd0db865bc6ea73d6899add6be475a6b7
cepiseptiyana <cepifams3@gmail.com>
2025-02-24 03:17:33 +0700

    path clear 404

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ProductView from "@/views/ProductView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/:pathMatch(.*)", component: NotFoundComponent },
     {
       path: "/",
       component: HomeView,
