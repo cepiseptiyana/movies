c2cbb08c4ea7a22f76931277409008cfdb141a6f
cepiseptiyana <cepifams3@gmail.com>
2025-03-13 12:47:26 +0700

    loading

--- src/views/MoviesNavView.vue
+++ src/views/MoviesNavView.vue
@@ -32,10 +32,47 @@ onBeforeRouteUpdate(async (to, from) => {
 
 <template>
   <AuthLayouts>
-    <h1 class="text-warning" v-if="loadElement">Loading ...</h1>
     <!-- Content -->
     <div class="content">
+      <h1 class="loader" v-if="loadElement"></h1>
       <content :movieHome="movieHome.data" />
     </div>
   </AuthLayouts>
 </template>
+
+<style scoped>
+.loader {
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  display: block;
+  color: #fff;
+  box-sizing: border-box;
+  animation: animloader 2s linear infinite;
+}
+
+@media only screen and (max-width: 576px) {
+  .loader {
+    width: 10px;
+    height: 10px;
+  }
+}
+
+@keyframes animloader {
+  0% {
+    box-shadow: 14px 0 0 -2px, 38px 0 0 -2px, -14px 0 0 -2px, -38px 0 0 -2px;
+  }
+  25% {
+    box-shadow: 14px 0 0 -2px, 38px 0 0 -2px, -14px 0 0 -2px, -38px 0 0 2px;
+  }
+  50% {
+    box-shadow: 14px 0 0 -2px, 38px 0 0 -2px, -14px 0 0 2px, -38px 0 0 -2px;
+  }
+  75% {
+    box-shadow: 14px 0 0 2px, 38px 0 0 -2px, -14px 0 0 -2px, -38px 0 0 -2px;
+  }
+  100% {
+    box-shadow: 14px 0 0 -2px, 38px 0 0 2px, -14px 0 0 -2px, -38px 0 0 -2px;
+  }
+}
+</style>
